a6c3c0fab380658d8107edb122d5188da6f39e34
Yohai <yohaido159@gmail.com>
2020-06-25 15:11:18 +0300

    Updates

---
+++ precache-manifest.127f366ae5a602465107cb6096f7d905.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41ba05bc104ea23c864fe7a074788aa1",
+    "url": "/lego4/index.html"
+  },
+  {
+    "revision": "1c6dd69d12da97a78e5f",
+    "url": "/lego4/static/css/main.e0dd08f1.chunk.css"
+  },
+  {
+    "revision": "d052719ea46f1d5a53db",
+    "url": "/lego4/static/js/2.c33bcce8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/lego4/static/js/2.c33bcce8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c6dd69d12da97a78e5f",
+    "url": "/lego4/static/js/main.d4892524.chunk.js"
+  },
+  {
+    "revision": "b58e94a55cb0649cae90",
+    "url": "/lego4/static/js/runtime-main.4b794664.js"
+  },
+  {
+    "revision": "f33bce28e1c42fb5e9cee1b22eae55c7",
+    "url": "/lego4/static/media/1.f33bce28.png"
+  },
+  {
+    "revision": "b775f425d4b7c2e05d8eea355510ed04",
+    "url": "/lego4/static/media/10.b775f425.png"
+  },
+  {
+    "revision": "8ce6b6c5f5b0b8924e319dcf228941fc",
+    "url": "/lego4/static/media/2.8ce6b6c5.png"
+  },
+  {
+    "revision": "46ee4b2f8660a85f56bf912ee346f433",
+    "url": "/lego4/static/media/3.46ee4b2f.png"
+  },
+  {
+    "revision": "61509327301fb79a1c9a21d9e2464b92",
+    "url": "/lego4/static/media/4.61509327.png"
+  },
+  {
+    "revision": "32d006d7ff13c85e8265e3221db27dd7",
+    "url": "/lego4/static/media/5.32d006d7.png"
+  },
+  {
+    "revision": "0c50e0f1a3e3c7e3ff71192aec405061",
+    "url": "/lego4/static/media/6.0c50e0f1.png"
+  },
+  {
+    "revision": "75de98bfb67f6131e2c2ad75ce2379eb",
+    "url": "/lego4/static/media/7.75de98bf.png"
+  },
+  {
+    "revision": "2b703085fada1286bc0ad3f7cc992d2d",
+    "url": "/lego4/static/media/8.2b703085.png"
+  },
+  {
+    "revision": "abaea0efc3c78d21b21d89f1f4107da5",
+    "url": "/lego4/static/media/9.abaea0ef.png"
+  },
+  {
+    "revision": "4dc5956a31b5832b356867433bb4e516",
+    "url": "/lego4/static/media/MyriadPro-Regular.4dc5956a.otf"
+  },
+  {
+    "revision": "2908e2d5ece93146516ce0cf7b1d6a52",
+    "url": "/lego4/static/media/Nehama.2908e2d5.ttf"
+  },
+  {
+    "revision": "cb5a21a92d77658df1beee4d51b72777",
+    "url": "/lego4/static/media/comic.cb5a21a9.ttf"
+  },
+  {
+    "revision": "8992f68960fee1ec24b769612913b9d0",
+    "url": "/lego4/static/media/hero1.8992f689.png"
+  }
+]);
